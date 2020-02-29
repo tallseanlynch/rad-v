76dc35706d11c0ab2338a024eb026c808f185f24
Sean Lynch <tallseanlynch@gmail.com>
2020-02-28 23:19:07 -0600

    UI style updates

--- src/components/app/storyExplorer.js
+++ src/components/app/storyExplorer.js
@@ -1,22 +1,36 @@
 const StoryExplorer = (cards) => {
     return `<!--Chapter section -->
-  <div class="chapter layer--main-elements block w-full h-full absolute z-index-2 bg-white">
+  <div class="chapter layer--main-elements block w-full h-full absolute z-index-2 bg-white p-8">
     <div class="story-explorer-container text-3xl">
-      <h1 class="font-bold text-5xl mb-8">Story Explorer<h1>
+      <h1 class="font-bold text-5xl mb-16 border-b-2 pb-4">Story Explorer<h1>
         <p><span class="font-bold">Number of Cards:</span> ${cards.cardInstances.length}</p>
         <p><span class="font-bold">Card Ids:</span>
-            ${cards.cardInstances.map(card => {
-                return StoryCardInstance(card)
-            }).join('')}
+            <div class="p-8">
+                ${cards.cardInstances.map(card => {
+                    return StoryCardInstance(card)
+                }).join('')}            
+            </div>
         <p>
 
       </div>
   </div>`
   }
 
+const sanitizeCardElementText = (cardElement) => {
+    if(typeof cardElement === 'string') {
+        return cardElement
+    } else {
+        if(cardElement.text) {
+            return cardElement.text
+        } else {
+            return 'UNKNOWN CARD ELEMENT TYPE'
+        }
+    }
+}
+
 const StoryCardInstance = (cardInstance) => {
-    return `<div class="story-card-instance block">
-        ${cardInstance.id}
+    return `<div class="story-card-instance block border-2 mb-4 p-2">
+        ${cardInstance.id} - ${sanitizeCardElementText(cardInstance.cardElements[0])}
     </div>`
 }
 
