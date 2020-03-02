9585e783cef109adaa472ad5b0646b5c4752ed7d
Sean Lynch <tallseanlynch@gmail.com>
2020-03-01 20:50:36 -0600

    Updated Improper Go Tos and added links for each

--- src/components/app/storyExplorer.js
+++ src/components/app/storyExplorer.js
@@ -1,9 +1,29 @@
+const improperGoTos = []
+const renderedImproperGoTos = (goTos) => `
+<div class="my-8 improper-go-tos">
+    <h2 class="font-bold">Improper Go Tos</h2>
+    ${goTos && goTos.map(goTo => `<p><a onClick="scrollCardIntoViewById('${goTo}')" class="text-red-500 italic">${goTo}<a></p>`).join('')}
+</div>
+`
+
+window.scrollCardIntoViewById = (el) => {
+    console.log(el)
+    document.getElementById(`id-${el}`).scrollIntoView();    
+}
+
+const renderImproperGoTos = () => {
+    if(document.querySelectorAll('.improper-go-tos')[0]) {
+        document.querySelectorAll('.improper-go-tos')[0].innerHTML = renderedImproperGoTos(improperGoTos)
+    }
+}
+
 const StoryExplorer = (cards) => {
     return `<!--Chapter section -->
   <div class="layer--main-elements block w-full absolute z-index-2 bg-white p-8">
     <div class="story-explorer-container text-3xl">
       <h1 class="font-bold text-5xl mb-16 border-b-2 pb-4">Story Explorer<h1>
         <p><span class="font-bold">Number of Cards:</span> ${cards.cardInstances.length}</p>
+        ${renderedImproperGoTos(improperGoTos)}
         <div class="my-8">
             ${cards.cardInstances.map(card => {
                 return StoryCardInstance(card)
@@ -27,20 +47,29 @@ const sanitizeCardElementText = (cardElement) => {
 
 const StoryCardInstance = (cardInstance) => {
 
-    const cardInstanceOptions = cardInstance.cardElements.filter(cardElement =>{
-        // console.log(cardElement)
-        return cardElement.goTo !== undefined
-    })
-
-    console.log('*********', cardInstanceOptions)
-
-    return `<div class="story-card-instance block border-2 mb-16 p-8 hover:bg-gray-100 hover:pl-4 relative">
-        ${cardInstance.cardElements.filter(ce => ce.goTo === undefined).map((ceText, ceTextIndex) => {
+    const cardInstanceStoryMoments = cardInstance.cardElements
+        .filter(ce => ce.goTo === undefined)
+        .map((ceText, ceTextIndex) => {
             return `<p class="py-2 ${ceTextIndex === 0 && 'font-bold'}"><em>${sanitizeCardElementText(ceText)}</em>${ceTextIndex === 0 ? `<em class="pl-4 text-gray-400">( ${cardInstance.id} )</em>` : ''}</p>`
-        }).join('')}
-        ${cardInstanceOptions.map(cardInstanceOption => {
-            return `<div class="my-4"><p><b> > ${cardInstanceOption.text ? cardInstanceOption.text : cardInstanceOption.template}</b><em class="pl-4 text-gray-400">( ${cardInstanceOption.goTo} )</em></p></div>`
-        }).join('')}
+        })
+        .join('')
+
+    const cardInstanceOptions = cardInstance.cardElements
+        .filter(cardElement => {
+            return cardElement.goTo !== undefined
+        })
+        .map(cardInstanceOption => {
+            const filterImproperGoTo = cardInstanceOption.goTo === 'card-instance-0-0' ? `<p><span class="text-red-500">IMPROPER GO TO<span></p>` : ''
+            filterImproperGoTo !== '' && improperGoTos.push(cardInstance.id)
+            setTimeout(renderImproperGoTos, 100)
+            console.log(improperGoTos)
+            return `<div class="my-4">${filterImproperGoTo}<p><b> > ${cardInstanceOption.text ? cardInstanceOption.text : cardInstanceOption.template}</b><em class="pl-4 text-gray-400">( ${cardInstanceOption.goTo} )</em></p></div>`
+        })
+        .join('')
+    
+    return `<div id="id-${cardInstance.id}" class="story-card-instance block border-2 mb-16 p-8 hover:bg-gray-100 hover:pl-4 relative">
+        ${cardInstanceStoryMoments}
+        ${cardInstanceOptions}
     </div>`
 }
 
